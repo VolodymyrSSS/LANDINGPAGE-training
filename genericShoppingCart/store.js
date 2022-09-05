2967d9e78061451a39271aa133ba5e65b75f8b13
Volodymyr Sych <volodymyr_sych@epam.com>
2022-09-05 19:27:26 +0300

    functionality to add new item and remove it were added

--- genericShoppingCart/store.js
+++ genericShoppingCart/store.js
@@ -10,30 +10,110 @@ if (document.readyState == 'loading') {
 }
 
 function ready() {
-  // отримаємо колекцію з двох кнопок по їх класу
+  // отримаємо колекцію кнопок видалення з корзини по їх класу
   var removeCartItemButtons = document.getElementsByClassName('btn-danger');
-  // пройдемось по колекції та отримавши кнопку навішаємо на неї подію
+  // пройдемось по колекції та отримавши кнопку, навішаємо на неї подію
   for (var i = 0; i < removeCartItemButtons.length; i++) {
-    var button = removeCartItemButtons[i]; // визначимо саму кнопку
-    button.addEventListener('click', removeCartItem);
+    var button = removeCartItemButtons[i]; // визначимо саму кнопку вартості
+    button.addEventListener('click', removeCartItem); // навісили подію 'по-кліку'
+  }
+
+  // отримаємо колекцію кнопок вибору кількості в корзині по їх класу
+  var quantityInputs = document.getElementsByClassName('cart-quantity-input');
+  // пройдемось по колекції та отримавши кнопку, навішаємо на неї подію
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i]; // визначимо саму кнопку кількості
+    input.addEventListener('change', quantityChanged); // навісили подію 'по-зміні'
+  }
+
+  // отримаємо колекцію кнопок додавання в корзину по їх класу
+  var addToCartButtons = document.getElementsByClassName('shop-item-button');
+  // пройдемось по колекції та отримавши саму кнопку, навішаємо на неї подію
+  for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i]; // визначимо саму кнопку додавання в корзину
+    button.addEventListener('click', addToCartClicked); // навісили подію 'по-кліку'
   }
 }
 
 function removeCartItem(event) {
-  buttonClicked = event.target; // визначимо ту кнопку, яка була натиснута
+  buttonClicked = event.target; // визначимо ту кнопку, по якій був клік
   // видаляємо кнопку з цілого ряда, бо вона в ряді має два батьківських елемента
   buttonClicked.parentElement.parentElement.remove();
-  updateCartTotal(); // також обновляєм загальну кількість після видалення
+  updateCartTotal(); // також обновляєм загальну вартість після видалення товару
+}
+
+function quantityChanged(event) {
+  var input = event.target; // визначимо той input, який був натиснутий
+  // перевірка на нечисло та від'ємне значення чи нуль
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1; // найменше число яке буде в полі кількості якщо 0 чи -1,-2...
+  }
+  updateCartTotal(); // також обновляєм загальну вартість після зміни кількості товару
+}
+
+function addToCartClicked(event) {
+  var button = event.target; // визначимо ту кнопку, по якій був клік
+  var shopItem = button.parentElement.parentElement; // отримати батьківський елемент, який містить усі потрібні поля для товару: name, img, price
+  var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText; // отримати значення назви
+  var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText; // отримати значення ціни
+  var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src; // отримати посилання/url на зображення в атрибуті src
+  addItemToCart(title, price, imageSrc); // додаємо товар в корзину функцією
+  updateCartTotal(); // також обновляєм загальну вартість після додавання товару в корзину
+}
+
+// для додавання товару в кошик потрібно створити цілий ряд у який передати параметри title, price, image
+function addItemToCart(title, price, imageSrc) {
+  var cartRow = document.createElement('div'); // створюємо html-елемент (ряд доданого товару, який містить потрібні значення)
+  // додаємо стилі до створеного html-елементу
+  cartRow.classList.add('cart-row');
+  var cartItems = document.getElementsByClassName('cart-items')[0]; // визначаєм місце в корзині куди додати ряд з доданим товаром
+  // у разі, якщо ми додаємо до корзини товар з тою самою назвою, то попередження
+  var cartItemNames = cartItems.getElementsByClassName('cart-item-title');
+  for (var i = 0; i < cartItemNames.length; i++) {
+    if (cartItemNames[i].innerText == title) {
+      alert('This item is already added to the cart');
+      return;
+    }
+  }
+  // потрібно створити точну копію (шаблон) блока з полями товару  через косі лапки
+  var cartRowContents = `
+    <div class="cart-item cart-column">
+        <img
+            class="cart-item-image"
+            src="${imageSrc}"
+            width="100"
+            height="100"
+        />
+        <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+        <input class="cart-quantity-input" type="number" value="1" />
+        <button class="btn btn-danger" type="button">REMOVE</button>
+    </div>
+  `;
+  cartRow.innerHTML = cartRowContents; // передаємо html-елемент-шаблон як контент
+  cartItems.append(cartRow); // додаємо ряд в кінець блока товарів кошика
+
+  // через те, що додали цей елемент після того, як сторінка була завантажена на початку,
+  // кнопки видалення та поле введення кількості немає в доданому елемені, тому
+  // потрібно кнопку видалення та поле введення кількості знайти спочатку та навішати на них відповідну подію
+  cartRow
+    .getElementsByClassName('btn-danger')[0]
+    .addEventListener('click', removeCartItem);
+  cartRow
+    .getElementsByClassName('cart-quantity-input')[0]
+    .addEventListener('change', quantityChanged);
 }
 
 // треба пройтись по усіх рядах, знайти вартість і помножити на кількість та відобразити
 function updateCartTotal() {
-  var total = 0; // для обчислення загальної вартості в корзині
   // спочатку знаходимо в цілому документі місце де є корзина та берем перший елемент
   // бо потрібно взяти тільки той де є ряди з ціною і кількістю, інші не потрібні
   var cartItemContainer = document.getElementsByClassName('cart-items')[0];
   //  визначаємо місце в самій корзині - де є ряди з ціною та кількістю
   var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+  var total = 0; // для обчислення загальної вартості в корзині
   // далі проходимось по елементам цього місця - по рядам
   for (var i = 0; i < cartRows.length; i++) {
     // визначаємо конкретний ряд, в якому містяться ціна та кількість
@@ -51,6 +131,8 @@ function updateCartTotal() {
     // визначаємо отримання загальної вартості
     total = total + price * quantity;
   }
+
+  total = Math.round(total * 100) / 100; // округляємо до двох знаків після коми
   // для відображ заг вартості знайдем в документі клас, візьмем перший елемент та його
   // текст i присвоїмо йому значення загальної вартості додавши знак долара попереду
   document.getElementsByClassName('cart-total-price')[0].innerText =
