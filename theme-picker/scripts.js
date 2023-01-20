f58462b0835be20396d2ada9de4c8197b8667b37
Volodymyr Sych <volodymyr_sych@epam.com>
2023-01-20 14:16:28 +0200

    change the state of the buttons and full styling functionality

--- theme-picker/scripts.js
+++ theme-picker/scripts.js
@@ -1,19 +1,23 @@
 /* we used the <button> element, the click event will also be called when using 
-the space and enter key to activate it. So we need:*/
+the space and enter key to activate it.*/
 
-// detect which button has been clicked
-
-// to select all the theme buttons on the page
+// select all the theme buttons on the page
 const themeSwitcher = document.querySelector('.theme-switcher'); // get the block where all theme buttons are placed
 const buttons = themeSwitcher.querySelectorAll('button'); // get all the theme buttons in that block
 
-// create the handler for event listener on button click
+// create the handler for event listener
 const handleThemeSelection = (event) => {
-  //   console.log('button clicked', event.target); // 1 - detect which button has been clicked
-  //   const theme = event.target.getAttribute('data-theme'); // 2 - target the data-theme value
-  //   console.log(theme); // 2 - log the data-theme value
-  const theme = event.target.getAttribute('data-theme');
-  document.documentElement.setAttribute('data-selected-theme', theme); // 3 -update the data-selected-theme property programatically
+  const target = event.target; // target button on event
+  const theme = target.getAttribute('data-theme'); // target the data-theme value
+  //   const isPressed = target.getAttribute('aria-pressed'); // target the aria-pressed value
+  document.documentElement.setAttribute('data-selected-theme', theme); // update the data-selected-theme property programatically
+
+  // to change the state of the buttons when clicking a button, we need:
+  const previouslyPressedButton = document.querySelector(
+    '[data-theme][aria-pressed="true"]'
+  ); // select the button that was still active
+  previouslyPressedButton.setAttribute('aria-pressed', 'false'); // then set aria-pressed to 'false'
+  target.setAttribute('aria-pressed', 'true'); // set its aria-pressed attribute to 'true',
 };
 
 // looping through buttons and adding a click event listener to each of them
