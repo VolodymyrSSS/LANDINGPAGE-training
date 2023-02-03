a3ed849b35d736437002b347aafdbdd19ad47b4f
Volodymyr Sych <volodymyr_sych@epam.com>
2023-02-03 11:51:39 +0200

    ability to send colors in URL

--- colorDepictService/script.js
+++ colorDepictService/script.js
@@ -36,17 +36,33 @@ function copyToClickboard(text) {
 https://gka.github.io/chroma.js/ по ссилці https://cdnjs.com/libraries/chroma-js встановлюємо через CDN в файлі index.html 
 Тобто, замість генерації самого кольору вручну, будемо використовувати цю бібліотеку використавши метод random() */
 
-function setRandomColors() {
-  cols.forEach((col) => {
+function setRandomColors(isInitial) {
+  // спочатку, isInitial = underfined
+  const colors = isInitial ? getColorsFromHash() : []; // якщо це початковий рендер, то взяти кольори
+
+  cols.forEach((col, index) => {
     // визначити, якщо замочок зачинений, то не міняти колір колонки (її клас)
     const isLocked = col.querySelector('i').classList.contains('fa-lock'); // визначити чи є клас для зачиненого замочка
 
     const colorTitle = col.querySelector('h2'); // DOM-елемент текст
     const button = col.querySelector('button'); // DOM-елемент кнопка
-    const color = chroma.random();
 
+    // якщо зміна кольору заблокована
     if (isLocked) {
+      colors.push(colorTitle.textContent);
       return;
+    }
+
+    // якщо це перша загрузка сторінки, то потрібрий колір беремо з хеша, або генеруємо колір бібл chroma
+    const color = isInitial
+      ? colors[index] // яукщо це кольори по індексу
+        ? colors[index] // беремо звідси значення потрібного кольору
+        : chroma.random() // рандомно генеруємо колір
+      : chroma.random(); // рандомно генеруємо колір
+
+    // якщо зміна кольору не заблокована i це не є першою загрузкою
+    if (!isInitial) {
+      colors.push(color); // якщо зміна кольору не заблокована
     }
 
     colorTitle.textContent = color;
@@ -55,6 +71,8 @@ function setRandomColors() {
     setTextColor(colorTitle, color);
     setTextColor(button, color);
   });
+
+  updateColorsHash(colors);
 }
 
 // для зміни кольору тексту якщо основний колір світлий і не видно букв тексту (коду в hex)
@@ -65,4 +83,24 @@ function setTextColor(text, color) {
   text.style.color = luminance > 0.5 ? '#000' : '#fff';
 }
 
-setRandomColors();
+// для можливості передачі обраних кольорів в мережу в хеші
+function updateColorsHash(colors = []) {
+  document.location.hash = colors
+    .map((col) => {
+      return col.toString().substring(1);
+    })
+    .join('-');
+}
+
+// для перевірки чи є щось в хеші щоб потім кінечний користувач міг згенерувати кольлори з хеша
+function getColorsFromHash() {
+  if (document.location.hash.length > 1) {
+    return document.location.hash
+      .substring(1) // забрати саму першу #
+      .split('-') // переведем в масив
+      .map((color) => '#' + color); // додаємо кожному кольору # та сам колір
+  }
+  return [];
+}
+
+setRandomColors(true); // берем кольора тільки при першій загрузці, бо потім будемо генерувати нові
