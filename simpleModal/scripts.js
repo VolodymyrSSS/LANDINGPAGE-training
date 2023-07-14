9caefaf56418cf022bbb46f2da7bc3929a8fff39
Volodymyr Sych <volodymyr_sych@epam.com>
2023-07-14 20:19:31 +0300

    fix added

--- simpleModal/scripts.js
+++ simpleModal/scripts.js
@@ -3,6 +3,7 @@ const modal = document.querySelector('#modal');
 const openModalButton = document.querySelector('#open-modal-btn');
 const closeModalButton = document.querySelector('#close-modal-btn');
 const overlay = document.querySelector('#overlay');
+let greetings = document.querySelector('.greetings');
 
 // event listener on click add class 'open' and opens modal
 openModalButton.addEventListener('click', () => {
@@ -29,3 +30,10 @@ function closeModal() {
   modal.classList.remove('open');
   overlay.classList.remove('open');
 }
+
+const greetingsList = ['Good Morning', 'Good Afternoon', 'Good Evening'];
+const currentHour = new Date().getHours();
+// determine the index of the appropriate greetingsList based on the current hour
+let index =
+  currentHour >= 12 && currentHour < 17 ? 1 : currentHour >= 17 ? 2 : 0;
+console.log(greetingsList[index]);
